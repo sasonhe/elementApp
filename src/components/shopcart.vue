a4c4b26a6d12a95632e5e9a9af125bda765de9b9
sasonhe <642139009@qq.com>
2018-04-03 22:18:59 +0800

    购物车组件

---
+++ src/components/shopcart.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="shopcart">
+  <div class="content">
+    <div class="content-left">
+      <div class="logo-wrapper">
+        <div class="logo">
+          <span class="icon-shopping_cart"></span>
+        </div>
+        <div class="num">
+
+        </div>
+      </div>
+      <div class="price">
+
+      </div>
+      <div class="desc">
+
+      </div>
+    </div>
+    <div class="content-right">
+      <div class="pay">
+
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.shopcart
+  position:fixed
+  left: 0
+  bottom: 0
+  z-index: 50
+  width: 100%
+  height: 48px
+  background: #ccc
+  .content
+    display:flex
+    font-size: 0
+    background: #141d27
+    .content-left
+      flex: 1
+      .logo-wrapper
+        display: inline-block
+      .price
+        display: inline-block
+      .desc
+        display: inline-block
+    .content-right
+      flex: 0 0 105px
+      width: 105px
+</style>
--- src/pages/goods.vue
+++ src/pages/goods.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="goods">
-  <div class="menu-wrapper">
+  <div class="menu-wrapper" ref="menuWrapper">
     <ul>
-      <li v-for="(item,index) in goods" class="menu-item">
+      <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}" ref="menuList" @click="selectMenu(index,$event)">
         <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
         </span>
       </li>
     </ul>
   </div>
-  <div class="foods-wrapper">
+  <div class="foods-wrapper" ref="foodsWrapper">
     <ul>
-      <li v-for="item in goods" class="foods-list">
+      <li v-for="item in goods" class="foods-list" ref="foodList">
         <h1 class="title">{{item.name}}</h1>
         <ul>
           <li v-for="food in item.foods" class="food-iten border-1px">
@@ -35,10 +35,13 @@
       </li>
     </ul>
   </div>
+  <shopcart></shopcart>
 </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll';
+import shopcart from '../components/shopcart.vue';
 export default {
   name: 'goods',
   props: {
@@ -48,17 +51,78 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
     }
   },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     this.$http.get("../static/selldata.json").then(response => {
       this.goods = response.data.goods;
-      // console.log(this.goods);
+      this.$nextTick(() => {
+        this._initScroll();
+        this._calculateHeight();
+      });
     }, response => {
       console.log("加载失败");
     })
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          this._followScroll(i);
+          return i;
+        }
+      }
+      return 0;
+    }
+  },
+  methods: {
+    selectMenu(index, event) {
+      if (!event._constructed) {
+        return;
+      }
+      let foodList = this.$refs.foodList;
+      let el = foodList[index];
+      this.foodsScroll.scrollToElement(el, 300);
+    },
+    _initScroll() {
+      this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      });
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        click: true,
+        probeType: 3
+      });
+      this.foodsScroll.on('scroll', (pos) => {
+        // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+        if (pos.y <= 0) {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        }
+      });
+    },
+    _calculateHeight() {
+      let foodList = this.$refs.foodList;
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    },
+    _followScroll(index) {
+      let menuList = this.$refs.menuList;
+      let el = menuList[index];
+      this.meunScroll.scrollToElement(el, 300, 0, -100);
+    }
+  },
+  components: {
+    shopcart
   }
 }
 </script>
@@ -82,6 +146,14 @@ export default {
       width: 56px
       padding: 0 12px
       line-height: 14px
+      &.current
+        position: relative
+        z-index: 10
+        margin-top: -1px
+        background: #fff
+        font-weight: 700
+        .text
+            border-none()
       .icon
         display: inline-block
         vertical-align: top
