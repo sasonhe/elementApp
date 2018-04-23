1547c301468b64792dda019c8a5cbe910ec44b41
sasonhe <642139009@qq.com>
2018-04-23 19:39:37 +0800

    add carControl

---
+++ src/components/carControl.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="carcontrol">
+
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
